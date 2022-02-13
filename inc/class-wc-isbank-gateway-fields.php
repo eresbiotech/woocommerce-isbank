25dc8fa4ff77697de23732bec469da7e13f0b8e4
Emre YILMAZ <z@emre.xyz>
2022-02-13 22:23:20 +0300

    translation update

--- inc/class-wc-isbank-gateway-fields.php
+++ inc/class-wc-isbank-gateway-fields.php
@@ -9,28 +9,28 @@ class WC_Isbank_Gateway_Fields {
 	public static function init_fields() {
 		return array(
 			'enabled'   => array(
-				'title' => __( 'Aktif/Deaktif', 'wc-isbank' ),
+				'title' => __( 'Enable', 'wc-isbank' ),
 				'type'  => 'checkbox',
-				'label' => __( 'Türkiye İş Bankası Sanal Pos etkinleştir', 'wc-isbank' )
+				'label' => __( 'Activate the payment method for IsBank', 'wc-isbank' )
 			),
 			'test'  => array(
-				'title' => __( 'Test ortamını aktif et', 'wc-isbank' ),
+				'title' => __( 'Enable test environment', 'wc-isbank' ),
 				'type'  => 'checkbox'
 			),
 			'client_id' => array(
-				'title' => __( 'Üye iş yeri numarası', 'wc-isbank' ),
+				'title' => __( 'IsBank Customer ID', 'wc-isbank' ),
 				'type'  => 'text'
 			),
 			'store_key' => array(
-				'title' => __( '3D Anahtarı', 'wc-isbank' ),
+				'title' => __( '3D Secret', 'wc-isbank' ),
 				'type'  => 'text'
 			),
 			'api_user'  => array(
-				'title' => __( 'API Kullanıcı Adı', 'wc-isbank' ),
+				'title' => __( 'API Username', 'wc-isbank' ),
 				'type'  => 'text'
 			),
 			'api_user_password' => array(
-				'title' => __( 'API Kullanıcı Parolası', 'wc-isbank' ),
+				'title' => __( 'API User password', 'wc-isbank' ),
 				'type'  => 'password'
 			)
 		);
